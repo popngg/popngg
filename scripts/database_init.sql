12bb7d5f43192daeb56a4f48ab64392d6b47b662
Minkyum Kim <37054037+39dll@users.noreply.github.com>
2025-08-01 04:30:54 +0900

    add test columns on chart table

--- scripts/database_init.sql
+++ scripts/database_init.sql
@@ -19,3 +19,9 @@ CREATE TABLE chart
     song_name  VARCHAR(255) NULL,
     created_at DATETIME     NULL
 ) ENGINE = INNODB;
+
+
+INSERT INTO chart VALUES (3, 0, 0, 49, 3, 1, 'TestGenreName1', 'TestJacket1', 'TestSongHash1', 'TestSongName1', '2023-06-01 00:00:00'),
+                         (4, 0, 1, 49, 10, 2, 'TestGenreName2', 'TestJacket2', 'TestSongHash2', 'TestSongName2', '2023-06-01 00:00:00'),
+                         (4, 1, 0, 49, 15, 3, 'TestGenreName3', 'TestJacket3', 'TestSongHash3', 'TestSongName3', '2023-06-01 00:00:00'),
+                         (4, 0, 0, 50, 15, 4, 'TestGenreName4', 'TestJacket4', 'TestSongHash4', 'TestSongName4', '2023-06-01 00:00:00');
