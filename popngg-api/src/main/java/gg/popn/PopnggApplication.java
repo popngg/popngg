7735e7a1eba9f47e76cff2e543cbc3c441b89589
Minkyum Kim <37054037+39dll@users.noreply.github.com>
2025-08-01 01:32:07 +0900

    initial commit

---
+++ popngg-api/src/main/java/gg/popn/PopnggApplication.java
@@ -0,0 +1,11 @@
+package gg.popn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopnggApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PopnggApplication.class, args);
+    }
+}
