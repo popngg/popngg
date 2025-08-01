7cdd0a8d6c794e6bc7a4a8fb220b456a24ff6abc
Minkyum Kim <37054037+39dll@users.noreply.github.com>
2025-08-01 13:47:19 +0900

    automate docker compose on local startup

---
+++ popngg-api/src/main/resources/scripts/database_init.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE IF NOT EXISTS popnggdb
+    DEFAULT CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
+
+USE popnggdb;
+
+CREATE TABLE chart
+(
+    difficulty INT          NULL,
+    is_deleted INT          NULL,
+    is_upper   INT          NULL,
+    level      INT          NULL,
+    version    INT          NULL,
+    chart_id   BIGINT AUTO_INCREMENT
+        PRIMARY KEY,
+    genre_name VARCHAR(255) NULL,
+    jacket     VARCHAR(255) NULL,
+    song_hash  VARCHAR(255) NULL,
+    song_name  VARCHAR(255) NULL,
+    created_at DATETIME     NULL
+) ENGINE = INNODB;
+
+
+INSERT INTO chart (difficulty, is_deleted, is_upper, level, version, genre_name, jacket, song_hash, song_name, created_at) VALUES (3, 0, 0, 49, 3, 'TestGenreName1', 'TestJacket1', 'TestSongHash1', 'TestSongName1', '2023-06-01 00:00:00'),
+                         (4, 0, 1, 49, 10, 'TestGenreName2', 'TestJacket2', 'TestSongHash2', 'TestSongName2', '2023-06-01 00:00:00'),
+                         (4, 1, 0, 49, 15, 'TestGenreName3', 'TestJacket3', 'TestSongHash3', 'TestSongName3', '2023-06-01 00:00:00'),
+                         (4, 0, 0, 50, 15, 'TestGenreName4', 'TestJacket4', 'TestSongHash4', 'TestSongName4', '2023-06-01 00:00:00');
