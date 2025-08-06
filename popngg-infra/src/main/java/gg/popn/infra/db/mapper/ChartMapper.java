c77517c9136073a04a8336f5e4665598e8ebcc89
Minkyum Kim <37054037+39dll@users.noreply.github.com>
2025-08-07 03:54:35 +0900

    implement chart mapper

---
+++ popngg-infra/src/main/java/gg/popn/infra/db/mapper/ChartMapper.java
@@ -0,0 +1,38 @@
+package gg.popn.infra.db.mapper;
+
+import gg.popn.domain.chart.model.Chart;
+import gg.popn.domain.chart.model.GroupedChart;
+import gg.popn.domain.chart.model.field.*;
+import gg.popn.infra.db.entity.ChartEntity;
+
+import java.util.List;
+
+public class ChartMapper {
+    public static Chart toDomain(ChartEntity chartEntity) {
+        return Chart.builder()
+                .songHash(SongHash.of(chartEntity.getSongHash()))
+                .genreName(GenreName.of(chartEntity.getGenreName()))
+                .songName(SongName.of(chartEntity.getSongName()))
+                .version(Version.of(chartEntity.getVersion()))
+                .difficulty(Difficulty.of(chartEntity.getDifficulty()))
+                .level(Level.of(chartEntity.getLevel()))
+                .isUpper(IsUpper.of(chartEntity.getIsUpper()))
+                .build();
+    }
+
+    public static ChartEntity toEntity(Chart chart) {
+        return ChartEntity.builder()
+                .songHash(chart.getSongHash().getValue())
+                .genreName(chart.getGenreName().getValue())
+                .songName(chart.getSongName().getValue())
+                .version(chart.getVersion().getValue())
+                .difficulty(chart.getDifficulty().getValue())
+                .level(chart.getLevel().getValue())
+                .isUpper(chart.getIsUpper().getValue())
+                .build();
+    }
+
+    public static List<GroupedChart> toGroupedChart(List<Chart> charts) {
+        return null; // TODO: implement
+    }
+}
