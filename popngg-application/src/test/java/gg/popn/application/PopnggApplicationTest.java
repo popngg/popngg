c047fe4dd4db4a8c4d01d4e9ea09ac1e741918b1
Minkyum Kim <37054037+39dll@users.noreply.github.com>
2026-07-24 15:23:20 +0900

    Add PR diff-based test coverage check (#13)

---
+++ popngg-application/src/test/java/gg/popn/application/PopnggApplicationTest.java
@@ -0,0 +1,5 @@
+package gg.popn.application;
+
+public class PopnggApplicationTest {
+    // Placeholder test to enable JaCoCo report generation
+}
---
+++ popngg-infra/src/test/java/gg/popn/infra/PopnggInfraTest.java
@@ -0,0 +1,5 @@
+package gg.popn.infra;
+
+public class PopnggInfraTest {
+    // Placeholder test to enable JaCoCo report generation
+}
