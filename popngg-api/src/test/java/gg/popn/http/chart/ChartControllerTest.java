19124c34194eb34fa3c6f5aee0d00c708bd246db
juneh2633 <juneh2633@gmail.com>
2026-09-24 12:01:43 +0900

    fix: return 404 for removed API routes

--- popngg-api/src/test/java/gg/popn/http/chart/ChartControllerTest.java
+++ popngg-api/src/test/java/gg/popn/http/chart/ChartControllerTest.java
@@ -1,6 +1,7 @@
 package gg.popn.http.chart;
 
 import gg.popn.application.chart.port.in.CreateChartUseCase;
+import gg.popn.http.common.exception.BaseExceptionHandler;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.util.List;
@@ -12,10 +13,12 @@ class ChartControllerTest {
     @Test
     void removedReadRoutesAreNotMapped() throws Exception {
         var create = mock(CreateChartUseCase.class);
-        var mvc = MockMvcBuilders.standaloneSetup(new ChartController(create)).build();
+        var mvc = MockMvcBuilders.standaloneSetup(new ChartController(create))
+                .setControllerAdvice(new BaseExceptionHandler()).build();
         for (String path : List.of("/api/v2/chart/all", "/api/v2/chart/recent",
                 "/api/v2/chart/song-hash", "/api/v2/chart/song-hash/4")) {
-            mvc.perform(get(path)).andExpect(status().isNotFound());
+            mvc.perform(get(path)).andExpect(status().isNotFound())
+                    .andExpect(jsonPath("$.code").value("NOT_FOUND"));
         }
         verifyNoInteractions(create);
     }
